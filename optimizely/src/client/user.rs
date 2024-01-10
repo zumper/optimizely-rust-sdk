31557ce0f33607c770d4d14e53400c9d8e1cd2dc
Matthieu Devlin <matt@zumper.com>
2024-01-10 14:22:19 -0800

    fix: switch from fasthash to murmur3

--- optimizely/src/client/user.rs
+++ optimizely/src/client/user.rs
@@ -1,6 +1,7 @@
 // External imports
-use fasthash::murmur3::hash32_with_seed as murmur3_hash;
+use murmur3::murmur3_32 as murmur3_hash;
 use std::collections::HashMap;
+use std::io::Cursor;
 
 // Imports from crate
 use crate::datafile::{Experiment, FeatureFlag, Variation};
@@ -185,7 +186,7 @@ impl UserContext<'_> {
 
         // To hash the bucket key it needs to be converted to an array of `u8` bytes
         // Use Murmur3 (32-bit) with seed
-        let hash_value = murmur3_hash(bucketing_key.as_bytes(), HASH_SEED);
+        let hash_value = murmur3_hash(&mut Cursor::new(bucketing_key), HASH_SEED).unwrap();
 
         // Bring the hash into a range of 0 to 10_000
         let bucket_value = ((hash_value as f64) / (u32::MAX as f64) * MAX_OF_RANGE) as u64;
